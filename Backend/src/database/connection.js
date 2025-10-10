aed5de258de12fd1abc416620c83cf3d44f61b00
Isaleo <isaleoguimaraes284@gmail.com>
2025-10-10 19:18:57 -0300

    Ajustes

--- Backend/src/database/connection.js
+++ Backend/src/database/connection.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: '.env.production' });
 
 // Configuração da conexão com o banco de dados
 const dbConfig = {
